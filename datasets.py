fix normalization bug!

datasets.py
@@ -27,28 +27,26 @@ class Corpus(Dataset):
         (removes 'words' such as '...' that actually have ET features!)
         """
         # string = string.replace(".", "")
-        string = re.sub(r'([\w ])(\.)+', r'\1', string)
+        string = re.sub(r'([\w ])(\.)+', r'\1', string.strip().lower())
         string = string.replace(",", "")
         string = string.replace("--", "")
-        string = string.replace("`", "")
         string = string.replace("''", "")
-        string = string.replace('"', "")
-        string = string.replace("' ", " ")
+        # string = string.replace("' ", "")
         # string = string.replace(" '", " ")
         string = re.sub(r"'\s?$", '', string)
-        # string = string.replace("*", "")
-        # string = string.replace("\\", "")
-        # string = string.replace(";", "")
         # string = string.replace("- ", " ")
-        # string = string.replace("/", "-")
-        # string = string.replace("!", "")
-        # string = string.replace("?", "")
-        string = re.sub(r'[?!/;*()\`:&]', '', string)
+        string = re.sub(r'[?!/;*()\\`:&"]', '', string)
 
         string = re.sub(r"\s{2,}", " ", string)
-        if string.endswith('.'):
-            string = string[:-1]
-        return string.strip().lower()
+
+        # remove '...' or "'" at start or end
+        string = re.sub(r"(^'|'$|^\.{1,3}|\.{1,3}$)", '', string)
+
+        # remove apostrophes that are not followed by an alphabet
+        # may be unnecessary though
+        # string = re.sub(r"'([^a-z])", r"\1", string)
+
+        return string
 
     def normalize_et(self):
         # We keep the NaN values at first so that it doesn't mess up
@@ -289,7 +287,7 @@ class CorpusAggregator(Dataset):
         print('Num of words in vocabulary:', len(self.vocabulary))
 
         if normalize_aggregate:
-            self.normalizer = MinMaxScaler()
+            self.normalizer = StandardScaler()
 
             # ugly way to "flatten" the values into shape (N, 5) though :(
             feature_values = []
@@ -298,8 +296,7 @@ class CorpusAggregator(Dataset):
             self.normalizer.fit(np.array(feature_values))
 
             # a bit inefficient to do this but oh well...
-            self.et_targets = [np.nan_to_num(self.normalizer.transform(s))
-                               for s in self.et_targets]
+            self.et_targets = [np.nan_to_num(s) for s in self.et_targets]
             self.et_targets_original = np.copy(self.et_targets)
             self.et_targets = [self.normalizer.transform(s)
                                for s in self.et_targets]
@@ -355,7 +352,8 @@ class _SplitDataset(Dataset):
 
 
 def print_normalizer_stats(caller, normalizer):
-    print('\n--- {} Normalizer Stats ---'.format(caller.__class__.__name__))
+    print('\n--- {} {} Normalizer Stats ---'.format(
+        caller.__class__.__name__, normalizer.__class__.__name__))
     if normalizer.__class__.__name__ == 'MinMaxScaler':
         print('min_:', normalizer.min_)
         print('scale_:', normalizer.scale_)

train.py
@@ -13,6 +13,7 @@ def iterate(dataloader, train=True):
     epoch_loss = 0.0
     # loss calculated on the real/original values (not scaled)
     epoch_loss_ = torch.Tensor([0, 0, 0, 0, 0])
+    loss_ = 0  # placeholder to avoid errors
     for i, (sentences, et_targets, et_targets_orig) in enumerate(dataloader):
         sentences = sentences.type(torch.LongTensor)
         if USE_CUDA:
@@ -25,19 +26,22 @@ def iterate(dataloader, train=True):
              for x in et_preds.detach().cpu().numpy()])
 
         # starting from the padding index, make the prediction values 0
-        for sent, et_pred in zip(sentences, et_preds):
+        for sent, et_pred, et_pred_inverse in zip(
+                sentences, et_preds, et_preds_inverse):
             try:
                 pad_start_idx = np.where(sent.cpu().numpy() == 0)[0][0]
             except IndexError:
                 pad_start_idx = None
             et_pred[pad_start_idx:] = 0
-            et_preds_inverse[pad_start_idx:] = 0
+            et_pred_inverse[pad_start_idx:] = 0
 
         loss = torch.sqrt(mse_loss(et_preds, et_targets))
+
         # calculate the loss PER FEATURE
-        loss_ = torch.Tensor(
-            [mse_loss(et_preds_inverse[:, i], et_targets_orig[:, i]).item()
-             for i in range(5)])
+        loss_ = torch.sqrt(torch.Tensor(
+            [mse_loss(et_preds_inverse[:, :, i],
+                      et_targets_orig[:, :, i]).item()
+             for i in range(5)]))
 
         if train:
             optimizer.zero_grad()
@@ -71,8 +75,8 @@ else:
 dataset = CorpusAggregator(corpus_list, args.normalize_aggregate)
 initial_word_embedding = init_word_embedding_from_word2vec(
     dataset.vocabulary.keys())
-# mse_loss = torch.nn.MSELoss()
-mse_loss = torch.nn.L1Loss()
+mse_loss = torch.nn.MSELoss()
+# mse_loss = torch.nn.L1Loss()
 
 print('--- PARAMETERS ---')
 print('Learning Rate:', INITIAL_LR)
@@ -107,8 +111,8 @@ for k, (train_loader, test_loader) in enumerate(dataset.split_cross_val()):
         e_te_losses.append(test_loss)
         e_te_losses_.append(test_loss_)
 
-        # print('k:', k, 'e:', e,
-        #       '{:.5f}'.format(train_loss), '{:.5f}'.format(test_loss))
+        print('k:', k, 'e:', e,
+              '{:.5f}'.format(train_loss), '{:.5f}'.format(test_loss))
         # print(train_loss_)
         # print(test_loss_)
 